717f7de57d96af7cf5021b9d96ce56cf84f909df
462620215 <462620215@qq.com>
2017-04-24 10:03:13 +0800

    Update docker-entrypoint.sh

--- dockerfile/docker-entrypoint.sh
+++ dockerfile/docker-entrypoint.sh
@@ -5,12 +5,16 @@ set -x
 app_name="${APP_NAME:-default}"
 build_file="${BUILD_FILE:-ROOT.war}"
 build_cmd="${BUILD_CMD:-clean package -Dmaven.test.skip=true}"
+environment="${ENVIRONMENT:-pom.xml.test}"
 
 mkdir project
 
 echo "yes" | svn co --username=${SVN_USER} --password=${SVN_PWD}  ${SVN_URL} project/${app_name}
 
 cd  project/${app_name}
+
+rm -rf pom.xml
+mv ${ENVIRONMENT} pom.xml
 
 ../../maven/bin/mvn ${build_cmd}
 
